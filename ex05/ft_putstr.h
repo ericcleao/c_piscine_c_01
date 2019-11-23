af63a9f4affe96a60c6324535e6a36507bb9738f
Eric Cerqueira Leao <ecerquei@student.42sp.org.br>
2019-11-23 08:26:47 -0300

    c_piscine_c_01 Add exercise 05
    - Print a string using just the 'write' function
    - Interate throw the string until find a '\0' character
    - Printing the string's characters while interate throw it
    
    Signed-off-by: Eric Cerqueira Leao <ecerquei@student.42sp.org.br>

---
+++ ex05/ft_putstr.h
@@ -0,0 +1,6 @@
+#ifndef FT_PUTSTR_H   // guardas de cabeçalho, impedem inclusões cíclicas
+#define FT_PUTSTR_H
+
+void ft_putstr(char *str);
+
+#endif
---
+++ ex05/main.c
@@ -0,0 +1,7 @@
+#include "ft_putstr.h"
+#include <stdio.h>
+
+int	main(void)
+{
+	ft_putstr("Eu amo a 42 Sao Paulo\n");
+}
