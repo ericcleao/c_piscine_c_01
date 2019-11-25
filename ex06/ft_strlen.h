881a126b8029652527b669a8023a40e152903fab
Eric Cerqueira Leao <ecerquei@student.42sp.org.br>
2019-11-25 11:16:38 -0300

    c_piscine_c_01 Add exercise 06
    - Interate throw the string until find a '\0' character
    - Return the value of the size variable that's equals to the value of
    the variable used to interate throw the string
    
    Signed-off-by: Eric Cerqueira Leao <ecerquei@student.42sp.org.br>

---
+++ ex06/ft_strlen.h
@@ -0,0 +1,6 @@
+#ifndef FT_STRLEN_H   // guardas de cabeçalho, impedem inclusões cíclicas
+#define FT_STRLEN_H
+
+int ft_strlen(char *str);
+
+#endif
---
+++ ex06/main.c
@@ -0,0 +1,7 @@
+#include "ft_strlen.h"
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("\"Testando\" size = %d\n", ft_strlen("Testando"));
+}
